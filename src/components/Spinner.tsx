49144ec76fa0986f27253157b9a4a7ed3ea7341e
Brian <brianpagal@gmail.com>
2023-05-16 11:15:55 +0800

    feat: 🎸 spinner component and useEffect fetch

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,15 @@
+const Spinner = () => {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div
+        className="animate-spin inline-block w-16 h-16 border-[3px] border-current border-t-transparent text-blue-600 rounded-full"
+        role="status"
+        aria-label="loading"
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,50 @@
+import axios from 'axios';
 import Toolbar from '../components/Toolbar';
+import { useEffect, useState } from 'react';
+import { Anime } from '../types/anime';
+import Spinner from '../components/Spinner';
+
+interface AnimeResponse {
+  data: Anime[];
+  meta: {
+    count: number;
+  };
+  links: {
+    first: string;
+    prev: string;
+    next: string;
+    last: string;
+  };
+}
 
 const Home = () => {
+  const [animeDataStatus, setAnimeDataStatus] = useState<
+    'idle' | 'loading' | 'resolved' | 'rejected'
+  >('idle');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setAnimeDataStatus('loading');
+      try {
+        const result = await axios.get<AnimeResponse>(
+          'https://kitsu.io/api/edge/anime'
+        );
+
+        setAnimeDataStatus('resolved');
+        // console.log('💖💛💙💜💚 result');
+        // console.log(result.data.data[0].attributes.titles.en);
+      } catch (error) {
+        setAnimeDataStatus('rejected');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (animeDataStatus === 'loading') {
+    return <Spinner />;
+  }
+
   return (
     <div className="text-center bg-blue-500 mt-9">
       <h1 className="text-3xl">Anime List</h1>
